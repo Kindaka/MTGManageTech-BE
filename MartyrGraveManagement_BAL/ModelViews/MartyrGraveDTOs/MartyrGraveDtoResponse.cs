b32fe95c6084e748615467657635a58a77709038
Nguyen Dang Khanh <khanndse161988@fpt.edu.vn>
2024-10-25 15:13:45 +0700

    update

--- MartyrGraveManagement_BAL/ModelViews/MartyrGraveDTOs/MartyrGraveDtoResponse.cs
+++ MartyrGraveManagement_BAL/ModelViews/MartyrGraveDTOs/MartyrGraveDtoResponse.cs
@@ -12,6 +12,9 @@ namespace MartyrGraveManagement_BAL.ModelViews.MartyrGraveDTOs
         public int MartyrId { get; set; }
         public int AreaId { get; set; }
         public string MartyrCode { get; set; }
+        public int RowNumber { get; set; }
+        public int MartyrNumber { get; set; }
+        public int AreaNumber { get; set; }
 
         public List<GraveImageDtoRequest> Images { get; set; } = new List<GraveImageDtoRequest>();
         public List<MartyrGraveInformationDtoResponse> MatyrGraveInformations { get; set; } = new List<MartyrGraveInformationDtoResponse>();
